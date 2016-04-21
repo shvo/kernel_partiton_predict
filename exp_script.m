41b93138755a67f6832560b616b03e49daa92035
shvo <shvowang@gmail.com>
2016-04-21 18:39:47 +0800

    add programs

---
+++ exp_script.m
@@ -0,0 +1,1 @@
+process_raw_data(input_file, output_file)
---
+++ process_raw_data.m
@@ -0,0 +1,26 @@
+function y = process_raw_data(input_file, output_file)
+
+A = importdata(input_file);
+
+[H_A, W_A] = size(A);
+
+% build label and feature set
+Data_Set_Label = zeros(H_A/11,1);
+Data_Set_Feature = zeros(H_A/11,5);
+for n = 1 : H_A/11
+   Index = 11*(n-1)+1:11*n;
+   [Y,I] = min(A(Index,W_A));
+   Data_Set_Label(n) = A(11*(n-1)+I,5);
+   Data_Set_Feature(n,:) = [A(11*(n-1)+I,1:4),A(11*(n-1)+I,6)];
+end
+
+extra = [7, 11, 11, 0, 3, 4];
+le = length(extra);
+MM = [ones(H_A/11,1),zeros(H_A/11,le-1)];
+NN = [extra;zeros(le-1,le)];
+Extra_Feature = MM * NN;
+Data_Set_Feature = [Data_Set_Feature,Extra_Feature];
+
+save(output_file,'Data_Set_Label','Data_Set_Feature');
+
+y = [];
